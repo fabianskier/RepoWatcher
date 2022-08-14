a8c41003ac5d536a85669aa59f9ebbeca6275417
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-13 21:46:01 -0400

    Initial Commit

---
+++ RepoWatcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RepoWatcher
+//
+//  Created by Oscar Cristaldo on 2022-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Oscar Cristaldo on 2022-08-13.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
